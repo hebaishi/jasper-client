22b9ce617fcdfde17bca5bd252427c29477483d8
hebaishi <hebaishi.husam@gmail.com>
2014-05-03 23:33:26 +0000

    Modified and tested weather.py

--- client/modules/Weather.py
+++ client/modules/Weather.py
@@ -1,8 +1,9 @@
 import random
 import re
 import feedparser
+import unicodedata
 
-WORDS = ["WEATHER"]
+WORDS = ["WEATHER","TODAY","TOMORROW"]
 
 URL="http://open.live.bbc.co.uk/weather/feeds/en/2643743/3dayforecast.rss"
 
@@ -17,9 +18,19 @@ def handle(text, mic, profile):
         profile -- contains information related to the user (e.g., phone number)
     """
     mic.say("Pulling up the weather.")
+    list=[];
     d=feedparser.parse(URL)
-    for x in xrange(0,3):
-	mic.say(d.entries[x].title)
+    if bool(re.search(r'\btoday\b', text, re.IGNORECASE)):
+	list=[0]
+    elif bool(re.search(r'\btomorrow\b', text, re.IGNORECASE)):
+	list=[1]
+    else:
+	list=[0, 1, 2]
+    for x in list:
+        s=unicodedata.normalize('NFKD',d.entries[x].title).encode('ascii','ignore')
+        s=re.sub(r'(\d+)C',r'\1 degrees celsius.',s)
+	s=re.sub(r'\(\d+F\)',r'',s)
+        mic.say(s)
 
 
 def isValid(text):
